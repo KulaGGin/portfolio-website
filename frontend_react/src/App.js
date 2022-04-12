d104d7d42298c672a66fbb7ae4906259952d6ed9
DESKTOP-T3794MB\Kulagin <sergeant.coolagin@gmail.com>
2022-04-12 19:58:40 +0300

    Remove testimonial component.

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
             <About />
             <Work />
             <Skills />
-            <Testimonial />
+            {/*<Testimonial />*/}
             <Contact />
             <Footer />
         </div>
