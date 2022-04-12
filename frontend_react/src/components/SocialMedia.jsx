070173b56911eacfa97b554ae8b964ad50dcf90f
DESKTOP-T3794MB\Kulagin <sergeant.coolagin@gmail.com>
2022-04-12 17:50:45 +0300

    Link appropriate social media. Make whole social media circle links, not just inner icons.

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsTwitter, BsGithub, BsStackOverflow } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa'
+
+import './SocialMedia.scss'
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
+      <a className="app__social-link" href="https://github.com/KulaGGin">
+        <div>
+          <BsGithub />
+        </div>
+      </a>
+      <a className="app__social-link" href="https://stackoverflow.com/users/6693304/kulaggin">
+        <div>
+          <BsStackOverflow />
+        </div>
+      </a>
+      <a className="app__social-link" href="https://twitter.com/SergKulaGGin">
       <div>
-        <BsTwitter />
+          <BsTwitter />
       </div>
-      <div>
-        <FaFacebookF />
-      </div>
-      <div>
-        <BsInstagram />
-      </div>
+      </a>
     </div>
   );
 };
