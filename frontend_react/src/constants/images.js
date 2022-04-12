c59ade0ecf1f8872ad4e13331895660f245ac28f
DESKTOP-T3794MB\Kulagin <sergeant.coolagin@gmail.com>
2022-04-12 19:16:20 +0300

    Add tools image to the header.

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -39,6 +39,7 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
+import tools from '../assets/tools.png';
 
 export default {
   avatar,
@@ -79,4 +80,5 @@ export default {
   nb,
   skype,
   spotify,
+  tools,
 };
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -27,57 +27,62 @@ headerPractices.push({image: images.agile, tooltip:"I follow best software devel
 const Header = () => (
   (
     <div className='app__header app__flex'>
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-        className="app__header-info"
-      >
-        <div className='app__header-badge'>
-          <div className='badge-cmp app__flex'>
-            <span>👋</span>
-            <div style={{ marginLeft: 20 }}>
-              <h2 >Hello, I am</h2>
-              <h1 className='head-text'>Sergei</h1>
-              <h2 >Nice to meet you.</h2>
+      <div className="app__header-main app__flex">
+        <motion.div
+          whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.5 }}
+          className="app__header-info"
+        >
+          <div className='app__header-badge'>
+            <div className='badge-cmp app__flex'>
+              <span>👋</span>
+              <div style={{ marginLeft: 20 }}>
+                <h2 >Hello, I am</h2>
+                <h1 className='head-text'>Sergei</h1>
+                <h2 >Nice to meet you.</h2>
+              </div>
+            </div>
+            <div className='tag-cmp app__flex'>
+              <p className='p-text'>
+                I'm a software developer based in Lviv.<br/>
+                I'm quietly confident, naturally curious, and perpetually improving my software craftsmanship chops every day.</p>
             </div>
           </div>
-          <div className='tag-cmp app__flex'>
-            <p className='p-text'>
-              I'm a software developer based in Lviv.<br/>
-              I'm quietly confident, naturally curious, and perpetually improving my software craftsmanship chops every day.</p>
-          </div>
-        </div>
-      </motion.div>
-      <motion.div
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
-        <img src={images.avatar} alt="profile_bg" />
-      </motion.div>
-      <motion.div
-        variants={scaleVariants}
-        whileInView={scaleVariants.whileInView}
-        className="app__header-circles"
-      >
-        {headerPractices.map((practice, index) => (
-          <div className="circle-cmp app__flex">
-            <div className="app__flex" data-tip="Placeholder Tooltip" data-for={`circle-${index}`} key={`circle-${index}`}>
-              <img src={practice.image} alt="profile_bg" />
-            </div>
+        </motion.div>
+        <motion.div
+          whileInView={{ opacity: [0, 1] }}
+          transition={{ duration: 0.5, delayChildren: 0.5 }}
+          className="app__header-img"
+        >
+          <img src={images.avatar} alt="profile_bg" />
+        </motion.div>
+        <motion.div
+          variants={scaleVariants}
+          whileInView={scaleVariants.whileInView}
+          className="app__header-circles"
+        >
+          {headerPractices.map((practice, index) => (
+            <div className="circle-cmp app__flex">
+              <div className="app__flex" data-tip="Placeholder Tooltip" data-for={`circle-${index}`} key={`circle-${index}`}>
+                <img src={practice.image} alt="profile_bg" />
+              </div>
 
-            <ReactTooltip
-              type="warning"
-              id={`circle-${index}`}
-              effect="solid"
-              arrowColor="#fff"
-              className="header-circle-tooltip"
-            >
-              {practice.tooltip}
-            </ReactTooltip>
-          </div>
-        ))}
-      </motion.div>
+              <ReactTooltip
+                type="warning"
+                id={`circle-${index}`}
+                effect="solid"
+                arrowColor="#fff"
+                className="header-circle-tooltip"
+              >
+                {practice.tooltip}
+              </ReactTooltip>
+            </div>
+          ))}
+        </motion.div>
+      </div>
+      <div className="app__header-footer">
+        <img className="app__header-footerImage" src={images.tools} alt="" />
+      </div>
     </div>
   )
 )
