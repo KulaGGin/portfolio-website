6f1e1547d68e7134fcc492b3b83c83216d2b1845
DESKTOP-T3794MB\Kulagin <sergeant.coolagin@gmail.com>
2022-04-14 16:48:17 +0300

    Use scss variable for the main class name in the Project component.

--- frontend_react/src/components/Project/Project.jsx
+++ frontend_react/src/components/Project/Project.jsx
@@ -3,28 +3,29 @@ import { urlFor } from "../../client";
 import { motion } from "framer-motion";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 
-import './Project.scss'
+import variables from './Project.scss';
 
 const Project = (props) => {
   const project = props.project;
+  const cn = variables.cn;
 
   return (
-    <div className='app__project app__flex'>
-      <div className='app__project-imageContainer app__flex'>
-        <img className="app__project-image" src={urlFor(project.imgUrl)} alt={project.name}/>
+    <div className={`${cn} app__flex`}>
+      <div className={`${cn}-imageContainer app__flex`}>
+        <img className={`${cn}-image`} src={urlFor(project.imgUrl)} alt={project.name}/>
         <motion.div
           whileHover={{ opacity: [0, 1] }}
           transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-          className="app__project-hover app__flex"
+          className={`${cn}-hover  app__flex`}
         >
           <a href={project.projectLink} target="_blank" rel="noreferrer">
             <motion.div
               whileInView={{ scale: [0, 1] }}
               whileHover={{ scale: [1, 0.9] }}
               transition={{ duration: 0.25 }}
-              className="app__project-linkIconContainer app__flex"
+              className={`${cn}-linkIconContainer  app__flex`}
             >
-              <AiFillEye className="app__project-linkIcon"/>
+              <AiFillEye className={`${cn}-linkIcon`}/>
             </motion.div>
           </a>
           <a href={project.codeLink} target="_blank" rel="noreferrer">
@@ -32,17 +33,17 @@ const Project = (props) => {
               whileInView={{ scale: [0, 1] }}
               whileHover={{ scale: [1, 0.9] }}
               transition={{ duration: 0.25 }}
-              className="app__project-linkIconContainer app__flex"
+              className={`${cn}-linkIconContainer app__flex`}
             >
-              <AiFillGithub className="app__project-linkIcon"/>
+              <AiFillGithub className={`${cn}-linkIcon`}/>
             </motion.div>
           </a>
         </motion.div>
       </div>
-      <div className="app__project-content app__flex">
-        <h4 className="app__project-title bold-text">{project.title}</h4>
+      <div className={`${cn}-content app__flex`}>
+        <h4 className={`${cn}-title bold-text`}>{project.title}</h4>
         <p className="p-text" style={{ marginTop: 10 }}>{project.description}</p>
-        <div className="app__project-tag app__flex">
+        <div className={`${cn}-tag app__flex`}>
           <p className="p-text">{project.tags[0]}</p>
         </div>
       </div>
