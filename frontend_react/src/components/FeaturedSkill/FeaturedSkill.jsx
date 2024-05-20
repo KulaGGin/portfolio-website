1200f13359dd956985c8fc0983bfdf05094b8107
DESKTOP-PCQMQK8\Kulagin <sergeant.coolagin@gmail.com>
2024-05-20 14:58:48 +0300

    Update FeaturedSkill component.

--- frontend_react/src/components/FeaturedSkill/FeaturedSkill.jsx
+++ frontend_react/src/components/FeaturedSkill/FeaturedSkill.jsx
@@ -35,9 +35,11 @@ const FeaturedSkill = (props) => {
       <div className="app__featuredSkill-main">
         <h2>{skill.name}</h2>
         <img src={urlFor(skill.icon)} alt={skill.name} className={`${styles.image}`} style={{ backgroundColor: skill.bgColor }} />
+        {skill.description !== undefined &&
         <TextContainer classNames={`${cn}-descriptionContainer`}>
           <p className={styles.description}>{skill.description}</p>
         </TextContainer>
+        }
       </div>
       <div className="app__featuredSkill-secondary">
         <div className="app__featuredSkill-projects">
@@ -61,13 +63,16 @@ const FeaturedSkill = (props) => {
               Click on an item to expand, or click on the Expand All button to expand all.
             </p>
           </TextContainer>
-          <div className="app__featuredSkill-indexList">
-            {
-              skill.indexTree.children.map((indexItem, numbfer) => (
-                traverseIndexTree(indexItem)
-              ))
-            }
-          </div>
+          <TextContainer classNames={`${cn}-indexContainer`}>
+            <div className="app__featuredSkill-indexList">
+              {
+                skill.indexTree.children.map((indexItem, numbfer) => (
+                  traverseIndexTree(indexItem)
+                ))
+              }
+            </div>
+          </TextContainer>
+
         </div>
         <div className="app__featuredSkill-educationalMaterials">
           <h2>Educational Materials</h2>
