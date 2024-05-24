602079108882dcbb4cf470e7b45b2823b7a82041
DESKTOP-PCQMQK8\Kulagin <sergeant.coolagin@gmail.com>
2024-05-24 13:34:13 +0300

    Update EducationalMaterial component.

--- frontend_react/src/components/EducationalMaterial/EducationalMaterial.jsx
+++ frontend_react/src/components/EducationalMaterial/EducationalMaterial.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { urlFor } from "../../client";
-import { motion } from "framer-motion";
-import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import {urlFor} from "../../client";
+import {motion} from "framer-motion";
+import {AiFillEye, AiFillGithub} from "react-icons/ai";
 
 import scssVars from './EducationalMaterial.scss';
 
@@ -10,24 +10,29 @@ const EducationalMaterial = (props) => {
   const cn = scssVars.cn;
 
   return (
-    <a className={`${props.classNames} ${cn}`} href={educationalMaterial.link}>
-      <img className={`${cn}-image`} src={urlFor(educationalMaterial.imgUrl)} alt={educationalMaterial.name}/>
-      <div className={`${cn}-content app__flex`}>
-        <h4 className={`${cn}-name`}>{educationalMaterial.name}</h4>
-        <p className={`${cn}-authors`}>
-          <>by </>
-          {
-            educationalMaterial.authors.reduce((author1, author2) => (
-              <>{author1}, {author2}</>
-            ))
-          }
-        </p>
-        <p className={`${cn}-description`}>{educationalMaterial.description}</p>
-        {/*<div className={`${cn}-tag app__flex`}>*/}
-        {/*  <p className="p-text">{educationalMaterial.tags[0]}</p>*/}
-        {/*</div>*/}
-      </div>
-    </a>
+      <a className={`${props.classNames} ${cn}`} href={educationalMaterial.link}>
+        {educationalMaterial.imgUrl &&
+            <img className={`${cn}-image`} src={urlFor(educationalMaterial.imgUrl)} alt={educationalMaterial.name}/>
+        }
+        <div className={`${cn}-content app__flex`}>
+          <h4 className={`${cn}-name`}>{educationalMaterial.name}</h4>
+          <p className={`${cn}-authors`}>
+            {educationalMaterial.authors &&
+                <>by </>
+            }
+
+            {educationalMaterial.authors &&
+                educationalMaterial.authors.reduce((author1, author2) => (
+                <>{author1}, {author2}</>
+                ))
+            }
+          </p>
+          <p className={`${cn}-description`}>{educationalMaterial.description}</p>
+          {/*<div className={`${cn}-tag app__flex`}>*/}
+          {/*  <p className="p-text">{educationalMaterial.tags[0]}</p>*/}
+          {/*</div>*/}
+        </div>
+      </a>
   );
 };
 
