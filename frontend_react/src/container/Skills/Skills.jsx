156c91560b47aad263a4cd7eca844ab7215ad1a6
DESKTOP-PCQMQK8\Kulagin <sergeant.coolagin@gmail.com>
2024-06-10 10:33:26 +0300

    Update Skills. Animate the skills when skill selection changes.

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -10,6 +10,8 @@ const Skills = () => {
   const [experience, setExperience] = useState([]);
   const [skills, setSkills] = useState([]);
   const [activeSkillIndex, setActiveSkillIndex] = useState(1);
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [currentSkill, setCurrentSkill] = useState(skills ? skills[0] : null);
 
   useEffect(() => {
     const query = '*[_type == "experiences"]';
@@ -21,20 +23,57 @@ const Skills = () => {
 
     client.fetch(skillsQuery).then((data) => {
       setSkills(data);
+      if(skills.length)
+        UpdateCurrentSkill(0);
     });
   }, []);
+
+  const HideCurrentSkill = (SkillIndex) => {
+    setActiveSkillIndex(SkillIndex);
+    setAnimateCard([{y:100, opacity: 0}])
+  }
+
+  const ShowCurrentSkill = (SkillIndex) => {
+    setAnimateCard([{y:0, opacity: 1}])
+
+    // if(WorkCategoryName === 'All') {
+    //   setFilterWork(works);
+    // } else {
+    //   setFilterWork(works.filter((work) => work.tags.includes(WorkCategoryName)))
+    // }
+  }
+
+  const SkillCategoryClicked_EventHandler = (SkillIndex) => {
+    HideCurrentSkill(SkillIndex);
+
+    setTimeout(() => {
+      UpdateCurrentSkill(SkillIndex);
+      ShowCurrentSkill(SkillIndex);
+    }, 500);
+  }
+
+  const UpdateCurrentSkill = (CurrentSkillIndex) => {
+    if(Array.isArray(skills) && skills.length) {
+
+      setCurrentSkill(skills[CurrentSkillIndex]);
+      // const indexTreeAssembler = new IndexTreeAssembler();
+      // skill.indexTree = indexTreeAssembler.assemble(skill.knowledgeIndex);
+    }
+  }
 
   return (
     <>
       <h2 className="head-text">Skills & Experience</h2>
       <div className="app__skills-container">
-        <motion.div className="app__skills-list">
+        <motion.div
+            transition={{ duration: 0.5, delayChildren: 0.5 }}
+            className="app__skills-list">
           {skills.map((skill, index) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className="app__skills-item app__flex"
-              onClick= { () => { setActiveSkillIndex(index) } }
+              onClick= { () => { SkillCategoryClicked_EventHandler(index) } }
               key={skill.name + index}
             >
               <div
@@ -43,23 +82,32 @@ const Skills = () => {
               >
                 <img src={urlFor(skill.icon)} alt={skill.name} />
               </div>
-              <p className="p-text">{skill.name}</p>
+              <p className="app__skills-item-text">{skill.name}</p>
             </motion.div>
           ))}
         </motion.div>
-        {
-          (() => {
-            if(Array.isArray(skills) && skills.length) {
-
-              let skill = skills[activeSkillIndex]
-
-              const indexTreeAssembler = new IndexTreeAssembler();
-              skill.indexTree = indexTreeAssembler.assemble(skill.knowledgeIndex);
-
-              return <FeaturedSkill skill={skill} />
-            }
-          })()
+        {currentSkill &&
+            <motion.div
+                animate={animateCard}
+                transition={{ duration: 0.5, delayChildren: 0.5 }}
+            >
+              <FeaturedSkill skill={currentSkill} />
+            </motion.div>
         }
+
+        {/*{*/}
+        {/*  (() => {*/}
+        {/*    if(Array.isArray(skills) && skills.length) {*/}
+
+        {/*      let skill = skills[activeSkillIndex]*/}
+
+        {/*      const indexTreeAssembler = new IndexTreeAssembler();*/}
+        {/*      skill.indexTree = indexTreeAssembler.assemble(skill.knowledgeIndex);*/}
+
+        {/*      return <FeaturedSkill skill={skill} />*/}
+        {/*    }*/}
+        {/*  })()*/}
+        {/*}*/}
       </div>
     </>
   );
