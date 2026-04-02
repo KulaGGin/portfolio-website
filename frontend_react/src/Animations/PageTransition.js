0277e00202ad44f6d66b259ad360d1834b310305
DESKTOP-839K1SS\Kulagin <sergeant.coolagin@gmail.com>
2026-04-02 19:39:16 +0200

    Add PageTransition animations.

---
+++ frontend_react/src/Animations/PageTransition.js
@@ -0,0 +1,21 @@
+export const PageVariants = {
+  initial: (direction) => ({
+    x: direction > 0 ? "100%" : "-100%",
+    opacity: 0,
+    scale: 0.98,
+  }),
+  animate: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+  },
+  exit: (direction) => ({
+    x: direction > 0 ? "-100%" : "100%",
+    opacity: 0,
+  }),
+};
+
+export const PageTransition = {
+  duration: 0.5,
+  ease: "easeInOut",
+};
