0af4286189f50b99c613b0e2ba70e41cf9abc163
DESKTOP-839K1SS\Kulagin <sergeant.coolagin@gmail.com>
2026-05-21 15:33:43 +0200

    Update HockeyGameProjectPage.

--- frontend_react/src/Pages/HockeyGameProjectPage/HockeyGameProjectPage.jsx
+++ frontend_react/src/Pages/HockeyGameProjectPage/HockeyGameProjectPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import Layout from "components/Layout/Layout";
+import scssVars from "./HockeyGameProjectPage.scss";
+import Logo from "./Images/GameLogo.png";
 
 const HockeyGameProjectPage = () => {
+  const cn = scssVars.cn;
   return (
-    <div>HockeyGameProjectPage</div>
+    <Layout>
+      <div className={`${cn}`}>
+        <div className={`${cn}__Header`}>
+          <img src={Logo} alt="Hockey Game Logo"/>
+        </div>
+      </div>
+    </Layout>
   )
 }
 
